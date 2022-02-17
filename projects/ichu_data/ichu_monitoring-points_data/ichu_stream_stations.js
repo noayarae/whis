02e5ffccec5ed93952b4a60538cd61129980f0c9
noayarae <enoay7@yahoo.com>
2022-02-17 13:03:17 -0800

    Add files via upload

---
+++ projects/ichu_data/ichu_monitoring-points_data/ichu_stream_stations.js
@@ -0,0 +1,23 @@
+var stream_points_json = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -74.977103,
+                    -12.786387
+                ]
+            }
+        }
+    ],
+    "properties": [
+        {
+            "Station_Co": "EH-01",
+            "Station_na": "Station_01",
+            "Agency": "National University of Huancavelica",
+            "State": "Huancavelica"
+        }
+    ]
+}
---
+++ projects/ichu_data/ichu_monitoring-points_data/ichu_weather.js
@@ -0,0 +1,103 @@
+var weather_points_json = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -74.979103,
+                    -12.789287
+                ]
+            }
+        },
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -74.99249,
+                    -12.7988
+                ]
+            }
+        },
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.037224,
+                    -12.790723
+                ]
+            }
+        },
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.101048,
+                    -12.85567
+                ]
+            }
+        },
+		{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.185596,
+                    -12.91668
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.090845,
+                    -13.035262
+                ]
+            }
+        }
+    ],
+    "properties": [
+        {
+            "Station_Co": "P01",
+            "Station_na": "Station_1",
+            "Source": "UNH",
+            "State": "Huancavelica"
+        },
+		{
+            "Station_Co": "P02",
+            "Station_na": "Station_2",
+            "Source": "UNH",
+            "State": "Huancavelica"
+        },
+		{
+            "Station_Co": "P03",
+            "Station_na": "Station_3",
+            "Source": "UNH",
+            "State": "Huancavelica"
+        },
+		{
+            "Station_Co": "P04",
+            "Station_na": "Station_4",
+            "Source": "UNH",
+            "State": "Huancavelica"
+        },
+		{
+            "Station_Co": "P05",
+            "Station_na": "Station_5",
+            "Source": "UNH",
+            "State": "Huancavelica"
+        },
+        {
+            "Station_Co": "P06",
+            "Station_na": "Station_6",
+            "Source": "UNH",
+            "State": "Huancavelica"
+        }
+    ]
+}
